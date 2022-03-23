d864a6b2e2522918b25bba527c6ebc2860dc0ddd
Caroline_SQ <cazawhoopy@gmail.com>
2022-03-23 15:40:43 +0000

    Select elements and add placeholders

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//List of the letters that have already been guessed Element
+const guessedLetters = document.querySelector(".guessed-letters");
+//Guess button Element
+const guessButton = document.querySelector (".guess");
+console.log(guessButton);
+//text input of a letter Element by user
+const inputLetter = document.querySelector (".letter");
+console.log(inputLetter);
+//word in progress element
+const wordIP = document.querySelector (".word-in-progress");
+//remaining guesses
+const remaining = document.querySelector ("remaining");
+//number of guesses number
+const remainingNum = document.querySelector ("span");
+console.log(remainingNum);
+// message after guess
+const message = document.querySelector ("message");
+//Play again button
+const paButton = document.querySelector (".play-again");
+console.log(paButton);
+console.log(wordIP);
+//starting word
+const word = "magnolia"
+
+//Function to add placeholders
+
+// 1 -creating an array of characters made of the letters of const word
+//console.log(word.split(""));
+
+const wordLetters = word.split("");
+console.log(wordLetters);
+
+
+//changing letters to circle symbols
+
+//2 - function to add a circle symbol for each letter in the array
+const symbolArray = wordLetters.map(function (item){
+  return item = "●";
+});
+console.log(symbolArray);
+
+//3 - adding circle symbol to paragrah
+wordIP.innerText = symbolArray.join("");
+
+
